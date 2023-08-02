e54eaf6a878b6ff7ca7c694949155f646cbcdd98
dev-tae <taehyun.kim0913@gmail.com>
2023-08-02 20:01:46 +0900

    Update urls.py to include new routes for chat and word of the day, create models for ChatMessage and WordOfTheDay, and update views to handle chat and word of the day features

--- back-end/english_mastery_app/core/models.py
+++ back-end/english_mastery_app/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class WordOfTheDay(models.Model):
+    word = models.CharField(max_length=200)
+    definition = models.TextField()
+    date = models.DateField(default=timezone.now)
+
+class ChatMessage(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
--- back-end/english_mastery_app/core/views.py
+++ back-end/english_mastery_app/core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Home Page")
+
+def chatbot(request):
+    return HttpResponse("Chatbot Page")
+
+def word(request):
+    return HttpResponse("Word of the Day Page")
